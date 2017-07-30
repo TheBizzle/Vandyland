5bc8c4a2d95afbea2eced8d5ac1176e5002cc1a1
TheBizzle <jason.bertsche@gmail.com>
2017-07-30 17:53:59 -0500

    Refactor: Unify "bad request" code; use HTTP 422

--- src/main/Main.hs
+++ src/main/Main.hs
@@ -6,7 +6,7 @@ import Control.Monad.IO.Class(liftIO)
 
 import Data.Text.Encoding(decodeUtf8)
 
-import Snap.Core(getParam, getPostParam, method, Method(GET, POST), route, Snap, writeText)
+import Snap.Core(getParam, method, Method(GET, POST), modifyResponse, route, setResponseStatus, Snap, writeText)
 import Snap.Http.Server(quickHttpServe)
 
 import NameGen(generateName)
@@ -31,15 +31,15 @@ handleListSession :: Snap ()
 handleListSession =
   do
     param <- getParam "session-id"
-    param |> ((fromMaybe "Missing parameter: session ID" ) >>> decodeUtf8 >>> writeText)
+    maybe (notifyBadParams "session ID") (decodeUtf8 >>> writeText) param
 
 handleDownloadItem :: Snap ()
 handleDownloadItem =
   do
-    param  <- getParam "session-id"
-    param2 <- getParam "item-id"
-    let params = param <> param2
-    params |> ((fromMaybe "Missing parameter: session ID or item ID" ) >>> decodeUtf8 >>> writeText)
+    sessionID <- getParam "session-id"
+    uploadID  <- getParam "item-id"
+    let params = sessionID <> uploadID
+    maybe (notifyBadParams "session ID or item ID") (decodeUtf8 >>> writeText) params
 
 handleUpload :: Snap ()
 handleUpload =
@@ -51,3 +51,9 @@ handleUpload =
     --upData  <- getPostParam "data"
     --let params = upImage <> upData
     --maybe (writeBS "must specify echo/param in URL") writeBS params
+
+notifyBadParams :: Text -> Snap ()
+notifyBadParams paramDesc =
+  do
+    modifyResponse $ setResponseStatus 422 "Unprocessable Entity"
+    writeText $ "Missing parameter(s): " <> paramDesc
