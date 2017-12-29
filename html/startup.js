499ace27b001348d1824efe269700bfd1f10c643
TheBizzle <jason.bertsche@gmail.com>
2017-12-28 20:01:39 -0600

    Feature: Give comments sanely-formatted timestamps

--- html/startup.js
+++ html/startup.js
@@ -15,12 +15,37 @@ window.hideModal = function() {
 
 let addCommentTo = function(commentsElem) { return function(comment) {
 
+  let formatTime = function(time) {
+
+    let millis  = Date.now() - new Date(time);
+
+    let minutes = Math.floor(millis  / 60000);
+    let hours   = Math.floor(minutes /    60);
+    let days    = Math.floor(hours   /    24);
+    let years   = Math.floor(days    /   365);
+
+    let modMinutes = minutes %  60;
+    let modHours   = hours   %  24;
+    let modDays    = days    % 365;
+
+    if (years !== 0) {
+      return `${years} years, ${modDays} days, ${modHours} hours, and ${modMinutes} minutes ago`;
+    } else if (days !== 0) {
+      return `${modDays} days, ${modHours} hours, and ${modMinutes} minutes ago`;
+    } else if (hours !== 0) {
+      return `${modHours} hours and ${modMinutes} minutes ago`;
+    } else {
+      return `${modMinutes} minutes ago`;
+    }
+
+  }
+
   let makeCommentElem =
     function({ comment, author, time }) {
       let template = document.getElementById("comment-template");
       template.content.querySelector(".comment-text"  ).textContent = comment;
       template.content.querySelector(".comment-author").textContent = author;
-      template.content.querySelector(".comment-time"  ).textContent = (Date.now() - new Date(time)) + " milliseconds ago";
+      template.content.querySelector(".comment-time"  ).textContent = formatTime(time);
       return document.importNode(template.content, true);
     };
 
